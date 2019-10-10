c04f310033f8fce703ad59ee15ce89562148c97d
Bob Wei <bob.wei@linecorp.com>
2019-10-10 12:14:50 +0800

    Update getFollowings to support recursive

--- src/apis/getFollowings/index.js
+++ src/apis/getFollowings/index.js
@@ -1,14 +1,34 @@
 import CookieManager from 'react-native-cookies';
 import axios from 'axios';
+import * as R from 'ramda';
 
-const fn = async (props = {}) => {
-  // prettier-ignore
+const fn = async (props = {}, output = {}) => {
   const { ds_user_id: defaultUserId } = await CookieManager.get('https://www.instagram.com');
   const { userId = defaultUserId, first = 12, after } = props;
   const variables = { id: userId, first, after };
-  // prettier-ignore
-  const url = `https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=${JSON.stringify(variables)}`;
-  return axios.get(url).then((res) => res.data);
+  if (props.hasNext === false) {
+    return output;
+  }
+  return axios
+    .get(getUrl(variables))
+    .then((res) => res.data)
+    .then(R.mergeDeepWithKey((k, l, r) => (k === 'edges' ? R.concat(l, r) : r), output))
+    .then((results) => {
+      if (!props.recursive) {
+        return results;
+      }
+      const { end_cursor: cursor, has_next_page: hasNext } = getPageInfo()(results);
+      return fn({ ...props, after: cursor, hasNext }, results);
+    });
 };
 
 export default fn;
+
+function getPageInfo() {
+  return R.path(['data', 'user', 'edge_follow', 'page_info']);
+}
+
+function getUrl(variables) {
+  // prettier-ignore
+  return `https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=${JSON.stringify(variables)}`;
+}
